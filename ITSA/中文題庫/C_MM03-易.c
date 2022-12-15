e3ba3ab92816085085ac2a651f15c84d7e10075e
林柏恩 <44471365+kimmiel@users.noreply.github.com>
2022-12-15 19:12:41 +0800

    Update C_MM03-易.c

--- ITSA/中文題庫/C_MM03-易.c
+++ ITSA/中文題庫/C_MM03-易.c
@@ -4,12 +4,13 @@ int main()
 {
     int num1[1000], num2[1000];
     int count = 0;
+    //讀入多組數字
     while (scanf("%d %d", &num1[count], &num2[count])!=EOF)
     {
 
         count++;
     }
-
+//輸出答案
     for (int i = 0; i < count; i++)
     {
         printf("%d\n", (num1[i] + num2[i]));
